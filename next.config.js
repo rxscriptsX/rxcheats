218a7857b565b407f2582fb9980281195b6264e9
rxscriptsX <contactnotifieralex@gmail.com>
2026-05-10 01:05:37 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn.discordapp.com', 'avatars.githubusercontent.com'],
+  },
+};
+
+module.exports = nextConfig;
