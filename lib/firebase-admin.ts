1074e992dac6539f7dfe89e2ccf44c8c5c398eab
rxscriptsX <contactnotifieralex@gmail.com>
2026-05-10 01:53:43 +0200

    Create firebase-admin.ts

---
+++ lib/firebase-admin.ts
@@ -0,0 +1,11 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+export const db = admin.database();
